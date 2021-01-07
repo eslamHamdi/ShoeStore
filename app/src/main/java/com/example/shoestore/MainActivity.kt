5d1344532a7ddff69c7323c23fa190b1dc48ac92
eslamHamdi <63453947+eslamHamdi@users.noreply.github.com>
2021-01-07 03:13:09 +0200

    first three fragments implementations

--- app/src/main/java/com/example/shoestore/MainActivity.kt
+++ app/src/main/java/com/example/shoestore/MainActivity.kt
@@ -2,12 +2,22 @@ package com.example.shoestore
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.drawerlayout.widget.DrawerLayout
+import com.example.shoestore.databinding.ActivityMainBinding
 import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var drawer:DrawerLayout
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+                //setContentView(R.layout.activity_main)
+
+        val binding :ActivityMainBinding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+
+        drawer = binding.drawer
+
         Timber.plant(Timber.DebugTree())
     }
 }
